f402843e947a77642d8e387b42cbdfc277b058dd
kaye <kaaboaye@gmail.com>
2017-11-07 19:39:56 +0100

    1.0

--- Clock/MainWindow.xaml.cs
+++ Clock/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Clock
 {
@@ -20,9 +21,22 @@ namespace Clock
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer Timer;
+
         public MainWindow()
         {
             InitializeComponent();
+            Timer = new DispatcherTimer();
+
+            Timer.Tick += new EventHandler(Timer_Click);
+            Timer.Interval = new TimeSpan(0, 0, 1);
+            Timer.Start();
+        }
+
+        private void Timer_Click(object sender, EventArgs e)
+        {
+            DateTime d = DateTime.Now;
+            TimeLabel.Text = $"{d.Hour}.{d.Minute}.{d.Second}";
         }
     }
 }
